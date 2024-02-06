0c645507e6923bc2cd5ae45f7b2a1f2e60675b75
bestga84 <bestga84@gmail.com>
2024-02-06 20:52:30 +0200

    02-video.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,49 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`
+);
+
+galleryList.insertAdjacentHTML("beforeend", markup.join(""));
+
+
+galleryList.addEventListener("click", onClick);
+
+let instance = basicLightbox.create("");
+
+function onClick(evt) {
+    evt.preventDefault();
+    const {target} = evt;
+    if (!target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    instance = basicLightbox.create(`<img class="gallery__image" 
+    src="${target.getAttribute("data-source")}" 
+    alt="${target.alt}"/>`);
+    instance.show();
+};
+
+galleryList.addEventListener("keydown", (evtClose) => {
+    if (evtClose.key === "Escape") {
+        instance.close();
+    };
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeEl);
+
+const savedCurrentTime = localStorage.getItem('videoplayer-current-time');
+setSavedCurrentTime();
+
+const currentVideoTime = data => {
+  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(currentVideoTime, 1000));
+
+function setSavedCurrentTime() {
+  if (savedCurrentTime !== null) {
+    player.setCurrentTime(savedCurrentTime);
+    return;
+  }
+  localStorage.getItem('videoplayer-current-time', '0');
+}
+
+console.log(localStorage.getItem('videoplayer-current-time'));
